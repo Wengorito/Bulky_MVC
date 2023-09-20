1dbd8bbd8c362599b4de949267fb2c09158f579e
Grzegorz <hvitr.ulfr@gmail.com>
2023-09-20 10:30:20 +0200

    Moved the files to their corresponding projects (N-Tier architecture)

--- Bulky.DataAccess/Data/ApplicationDbContext.cs
+++ Bulky.DataAccess/Data/ApplicationDbContext.cs
@@ -1,7 +1,7 @@
-﻿using BulkyWeb.Models;
+﻿using Bulky.Models;
 using Microsoft.EntityFrameworkCore;
 
-namespace BulkyWeb.Data
+namespace Bulky.DataAccess.Migrations
 {
     public class ApplicationDbContext : DbContext
     {
