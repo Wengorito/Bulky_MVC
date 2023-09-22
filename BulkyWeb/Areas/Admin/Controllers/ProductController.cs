757a35dc7c1e9bbc62b7cf272ba1ad4424dcd3f5
Grzegorz <hvitr.ulfr@gmail.com>
2023-09-22 16:53:38 +0200

    Saving Files (Images) from the Form

--- BulkyWeb/Areas/Admin/Controllers/ProductController.cs
+++ BulkyWeb/Areas/Admin/Controllers/ProductController.cs
@@ -11,10 +11,12 @@ namespace BulkyBook.Web.Areas.Admin.Controllers
     public class ProductController : Controller
     {
         private readonly IUnitOfWork _unitOfWork;
+        private readonly IWebHostEnvironment _webHostEnvironment;
 
-        public ProductController(IUnitOfWork unitOfWork)
+        public ProductController(IUnitOfWork unitOfWork, IWebHostEnvironment webHostEnvironment)
         {
             _unitOfWork = unitOfWork;
+            _webHostEnvironment = webHostEnvironment;
         }
 
         public IActionResult Index()
@@ -40,10 +42,12 @@ namespace BulkyBook.Web.Areas.Admin.Controllers
 
             if (id is null || id == 0)
             {
+                // create
                 vm.Product = new Product();
             }
             else
             {
+                // update
                 vm.Product = _unitOfWork.Product.Get(u => u.Id == id)!;
             }
 
@@ -53,13 +57,23 @@ namespace BulkyBook.Web.Areas.Admin.Controllers
         [HttpPost]
         public IActionResult Upsert(ProductVM vm, IFormFile? file)
         {
-            if (vm.Product.Price100 > vm.Product.Price50)
-            {
-                ModelState.AddModelError("", "Price 50+ should be higher than price 100+.");
-            }
-
             if (ModelState.IsValid)
             {
+                var wwwRootPath = _webHostEnvironment.WebRootPath;
+
+                if (file != null)
+                {
+                    string fileName = Guid.NewGuid().ToString() + Path.GetExtension(file.FileName);
+                    string productPath = Path.Combine(wwwRootPath, @"images\product");
+
+                    using (var fileStream = new FileStream(Path.Combine(productPath, fileName), FileMode.Create))
+                    {
+                        file.CopyTo(fileStream);
+                    }
+
+                    vm.Product.ImageUrl = @"\images\product\" + fileName;
+                }
+
                 _unitOfWork.Product.Add(vm.Product);
                 _unitOfWork.Save();
 
